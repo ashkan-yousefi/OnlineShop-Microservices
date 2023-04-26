6c258dbb472bdb8625b7a204b8b08530fed8ef67
ashkandeveloper5 <108582477+ashkandeveloper5@users.noreply.github.com>
2023-04-26 10:58:46 -0700

    Add UnitTestTo Project

---
+++ tests/ServiceTests/AccountTests/Account.UnitTest/ControllerTests/AccountControllerTest.cs
@@ -0,0 +1,41 @@
+﻿using Account.Api.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Moq;
+using Account.Service.Services.UserServices;
+using Account.Service.Interfaces.UserInterfaces;
+using Account.UnitTest.MoqData;
+using Account.Service.UOW;
+using Account.Repository.Context;
+using Microsoft.EntityFrameworkCore;
+using MassTransit.Transports;
+using AutoMapper;
+using Account.Api.GrpcServices;
+using Microsoft.Extensions.Logging;
+using System.Net.Http;
+using Microsoft.Extensions.Configuration;
+
+namespace Account.UnitTest.ControllerTests
+{
+    public class AccountControllerTest
+    {
+        //[Theory]
+        ////[InlineData]
+        //public void GetUser_When_UserIdString(string userEmail)
+        //{
+        //    //arrange
+        //    var mock = new Mock<IUserService>();
+        //    mock.Setup(m => m.GetAsync(userEmail).Result[0]).Returns(AccountMoqData.GetUser());
+        //    AccountController accountController = new AccountController()
+
+        //    //act
+
+
+        //    //assert
+        //}
+    }
+}
---
+++ tests/ServiceTests/AccountTests/Account.UnitTest/MoqData/AccountMoqData.cs
@@ -0,0 +1,17 @@
+﻿using Account.Core.Entities.User;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Account.UnitTest.MoqData
+{
+    public static class AccountMoqData
+    {
+        public static User GetUser()
+        {
+            return new User { Id = "123451234512345@ExampleId", Email = "Example@gmail.com", IsActive = true, FirstName = "Exaple", UserName = "Example", LastName = "Example", Password = "8C-FA-22-82-B1-7D-E0-A5-98-C0-10-F5-F0-10-9E-7D" };
+        }
+    }
+}
